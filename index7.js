b9e1e3d910c971f6b6a98322b57ff557435135a9
shubhammahto007 <shubham_24ds035@saitm.ac.in>
2024-12-03 15:51:44 +0530

    Add files via upload

---
+++ index7.js
@@ -0,0 +1,51 @@
+// console.log("----suprice_mf----");
+// const title = document.
+// getElementById("title");
+// console.log(title);
+// title.innerText ="hii iam shubham";
+
+const heading = document.getElementsByClassName("heading");
+heading.length;
+for(let i = 0;i<heading.length;i++){
+    if(i%2 !== 0)
+heading[i].innerText = "odd";
+    else 
+    heading[i].innerText = "even";
+}
+// Array.length;
+// for(let i = 10;i>= Array.length;i--){
+//     console.log(i);
+// }
+const arr = [10,9,8,7,6,5,4,3,2,1];
+const firstElement = arr[0];
+for(let i = 0; i<10;i++){
+    if(i%2 !==0){
+        arr[i]
+    console.log(i+1)}
+}
+const ar = [10,9,8,7,6,5,4,3,2,1];
+ar.lemgth;
+for(let i =0 ; i<ar.length;i++){
+    sum = 10+9+8+7+6+5+4+3+2+1;
+    console.log(sum)
+}
+
+
+
+
+
+
+
+
+
+
+// heading[0].innerText = "mere babu ne thana thaya ";
+// heading[1].innerText = "nahi thaya kyu nhi thaya oooooooo";
+// heading[2].innerText = "babu m__d---c--";
+// heading[3].innerText = "sorry babu menu ye gala nahi pata hondi";
+// heading[4].innerText = "-oyy kis ne touch kiya usko";
+// heading[5].innerText = "-oy unchle ji touda dimag tougda getiya wich hai kya ";
+
+/*const h1Ele = document.getelementbyid("heading");
+const newcontant  = "new line";
+h1Ele.innertext = newcontant;*/
